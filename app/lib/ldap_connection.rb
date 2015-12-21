2a9dc98368ad998b57f7ca53c2cadeccb342ef97
John Topley <john.topley@ons.gov.uk>
2015-12-21 14:18:41 +0000

    CTPA-186 Added comments. The returned user_entry should exist outside the LDAP#open block

--- app/lib/ldap_connection.rb
+++ app/lib/ldap_connection.rb
@@ -12,6 +12,7 @@ class LDAPConnection
   end
 
   def bind(username, password)
+    user_entry = nil
     auth = { method: :simple, username: username, password: password }
 
     # TODO Auth seems to accept any password.
@@ -24,19 +25,23 @@ class LDAPConnection
       @@logger.info "LDAP authentication succeeded for '#{username}'"
       user_entry = entry_for(username, ldap) || nil
 
+      # The user must be a member of at least the "<zone>-users" group for authentication to succeed.
       users_group = @@groups['users']
       unless group_member?(users_group, username, ldap)
         @@logger.error "LDAP authentication failed: '#{username}' is not a member of the '#{users_group}' group"
         return nil
       end
 
+      # See if the user is also a member of the "<zone>-admins" group.
       user_entry.admin = group_member?(@@groups['admins'], username, ldap)
-      user_entry
     end
+
+    user_entry
   end
 
   private
 
+  # Returns the LDAP directory entry for a user.
   def entry_for(username, ldap)
     filter = Net::LDAP::Filter.construct("(cn=#{username})")
     user_entry = nil
@@ -48,6 +53,7 @@ class LDAPConnection
     user_entry
   end
 
+  # Returns whether a user is a member of a group.
   def group_member?(group, username, ldap)
     filter = Net::LDAP::Filter.construct("(&(cn=#{group})(memberUid=#{username}))")
     group_cn = nil
