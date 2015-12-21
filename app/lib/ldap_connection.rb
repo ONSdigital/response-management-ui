820b0315b4cb6a102e7643927c0f2063ce00b885
John Topley <john.topley@ons.gov.uk>
2015-12-21 14:05:18 +0000

    CTPA-186 Successfully checking group membership for the authenticated user

--- app/lib/ldap_connection.rb
+++ app/lib/ldap_connection.rb
@@ -1,10 +1,8 @@
 require 'net/ldap'
 
-UserEntry = Struct.new(:display_name, :token)
+UserEntry = Struct.new(:display_name, :token, :admin)
 
 class LDAPConnection
-  TOKEN_ATTRIBUTE = 'employeeNumber'
-
   def initialize(host, port, base, groups, logger)
     @@host   = host
     @@port   = port.to_i
@@ -14,39 +12,46 @@ class LDAPConnection
   end
 
   def bind(username, password)
-    @ldap = Net::LDAP.new(host: @@host, port: @@port, base: @@base)
-    @ldap.auth(@@base, password)
-    entries = @ldap.bind_as(filter: "(cn=#{username})", password: password)
+    auth = { method: :simple, username: username, password: password }
+
+    # TODO Auth seems to accept any password.
+    Net::LDAP.open(host: @@host, port: @@port, base: @@base, auth: auth) do |ldap|
+      # unless ldap.bind
+      #   @@logger.error "LDAP authentication failed for '#{username}'"
+      #   return nil
+      # end
+
+      @@logger.info "LDAP authentication succeeded for '#{username}'"
+      user_entry = entry_for(username, ldap) || nil
+
+      users_group = @@groups['users']
+      unless group_member?(users_group, username, ldap)
+        @@logger.error "LDAP authentication failed: '#{username}' is not a member of the '#{users_group}' group"
+        return nil
+      end
+
+      user_entry.admin = group_member?(@@groups['admins'], username, ldap)
+      user_entry
+    end
+  end
+
+  private
+
+  def entry_for(username, ldap)
+    filter = Net::LDAP::Filter.construct("(cn=#{username})")
     user_entry = nil
 
-    if entries && entries.any?
-      @@logger.info "LDAP bind succeeded for '#{username}'"
-      return nil unless group_member?(@@groups['users'], username)
-
-      display_name = entries.first.displayName.first
-      token = entries.first.send(TOKEN_ATTRIBUTE.to_sym).first
-      user_entry = UserEntry.new(display_name, token)
-    else
-      @@logger.info "LDAP bind failed for '#{username}'"
+    ldap.search(filter: filter, attributes: ['displayName', 'employeeNumber']) do |entry|
+      user_entry = UserEntry.new(entry.displayName.first, entry.employeeNumber.first)
     end
 
     user_entry
   end
 
-  private
-
-  def group_member?(group, username)
+  def group_member?(group, username, ldap)
     filter = Net::LDAP::Filter.construct("(&(cn=#{group})(memberUid=#{username}))")
-    puts filter
-    @ldap.search(filter: filter) do |entry|
-      entry.each do |attribute, values|
-        puts "   #{attribute}:"
-          values.each do |value|
-            puts "      --->#{value}"
-        end
-      end
-    end
-    puts @ldap.get_operation_result
-    false
+    group_cn = nil
+    ldap.search(filter: filter, attributes: ['cn']) { |entry| group_cn = entry.cn.first }
+    group_cn == group
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ require 'rotp'
 
 class User
   attr_reader :display_name
+  attr_reader :admin
+  alias_method :admin?, :admin
 
   def self.authenticate(ldap_connection, params = {})
     username = params[:username]
@@ -9,12 +11,13 @@ class User
     return nil if username.blank? || password.blank?
 
     user_entry = ldap_connection.bind(username, password)
-    User.new(user_entry.display_name, user_entry.token) unless user_entry.nil?
+    User.new(user_entry.display_name, user_entry.token, user_entry.admin) unless user_entry.nil?
   end
 
-  def initialize(display_name, token)
+  def initialize(display_name, token, admin)
     @display_name = display_name
     @token        = token
+    @admin        = admin
   end
 
   def valid_code?(drift, params = {})
