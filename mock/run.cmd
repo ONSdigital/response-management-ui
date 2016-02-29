43fdc9e0307cb903f681f2332742e1eb4b41e1d6
John Topley <john.topley@ons.gov.uk>
2016-02-29 11:27:46 +0000

    Removed Windows command script as now running on Linux

--- mock/run.cmd
+++
@@ -1,4 +0,0 @@
-:: Batch file to start the mock follow-up and mock frame services on ports 8177 and 8178 respectively.
-@echo off
-start "Mock Follow-Up Service" cmd /c "cd .\followupservice && rackup -p 8177"
-start "Mock Frame Service" cmd /c "cd .\frameservice && rackup -p 8178"
