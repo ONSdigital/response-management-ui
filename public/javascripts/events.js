70efe13569b2f842b8108bb3027823555c88c1b7
Phil Sharland <philip.sharland@ons.gov.uk>
2016-03-22 10:26:36 +0000

    CTPA-232 - changes to case events - additional categories

--- public/javascripts/events.js
+++ public/javascripts/events.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('#eventtext').keyup(function() {
   var length = $(this).val().length;
-  var length = 100-length;
+  var length = 250 -length;
   $('#chars').text(length);
 });
 });
