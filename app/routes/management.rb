341eca06be8c6eb2a54245e6c59f127244774665
Phil Sharland <philip.sharland@ons.gov.uk>
2016-03-30 10:48:28 +0100

    CTPA-273 Add Escalation link to manage screen

--- app/routes/management.rb
+++ app/routes/management.rb
@@ -32,131 +32,13 @@ module Beyond
         erb :manage, locals: { title: 'Manage' }
       end
 
-      # Get all regions.
-      get '/manage/regions/?' do
+      # Get all escalated cases.
+      get '/manage/escalated' do
         authenticate!
         regions = JSON.parse(RestClient.get("http://#{settings.frame_service_host}:#{settings.frame_service_port}/frameservice/regions")).paginate(page: params[:page])
-        erb :manage_regions, locals: { title: 'Survey Access Token Management: Regions', regions: regions }
+        erb :escalated, locals: { title: 'View Escalated Cases' }
       end
 
-      # Get all LAs for the selected region.
-      get '/manage/regions/:region_code/las/?' do |region_code|
-        authenticate!
-        local_authorities = JSON.parse(RestClient.get("http://#{settings.frame_service_host}:#{settings.frame_service_port}/frameservice/lads?regionid=#{region_code}")).paginate(page: params[:page])
-        erb :manage_local_authorities, locals: { title: "Survey Access Token Management: Local Authorities for Region #{region_code}",
-                                                 region_code: region_code,
-                                                 local_authorities: local_authorities }
-      end
-
-      # Get all msoas and associated unactivated questionnaire counts for the selected LA.
-      get '/manage/regions/:region_code/las/:local_authority_code/msoas' do |region_code, local_authority_code|
-        authenticate!
-        msoas = JSON.parse(RestClient.get("http://#{settings.frame_service_host}:#{settings.frame_service_port}/frameservice/msoas?ladid=#{local_authority_code}&questionnairecounts=true")).paginate(page: params[:page])
-        erb :manage_msoas, locals: { title: "Survey Access Token Management: MSOAs for LA #{local_authority_code}",
-                                     region_code: region_code,
-                                     local_authority_code: local_authority_code,
-                                     msoas: msoas }
-      end
-
-      get '/manage/responsegenerator/?' do
-        authenticate!
-        rg_service = ResponseGenerator.new(generator_host: settings.respgen_service_host, generator_port: settings.respgen_service_port)
-        rgi = rg_service.read
-        erb :manage_response_generator, locals: { title: 'Response Generator Control', response_generator: rgi }
-      end
-
-      post '/manage/responsegenerator/?' do
-        authenticate!
-        rg_service = ResponseGenerator.new(generator_host: settings.respgen_service_host, generator_port: settings.respgen_service_port)
-        rgi = ResponseGeneratorInstruction.new
-        rgi.active = params['activeValue'] == 'Yes' ? true : false
-        rgi.responses_per_minute = params['responses_per_minuteValue']
-        rgi.run_until = Date.today.to_s + ' ' + params['run_untilValue']
-        rgi.filter = params['filterName']
-
-        begin
-          result = rg_service.save(rgi)
-          unless result.nil?
-            flash[:notice] = 'Successfully reconfigured the Response Generator.'
-          end
-        rescue PG::Error => err
-          flash[:error] = "Unable to reconfigure the Response Generator (#{err})"
-        end
-
-        redirect '/manage/responsegenerator'
-      end
-
-      # Activate the questionnaires within the selected MSOA.
-      put '/manage/regions/:region_code/las/:local_authority_code/msoas/:msoa_code' do |region_code, local_authority_code, msoa_code|
-        authenticate!
-        RestClient.patch("http://#{settings.frame_service_host}:#{settings.frame_service_port}/frameservice/msoas/#{msoa_code}/activate",
-                         {}.to_json, content_type: :json, accept: :json) do |response, _request, _result, &_block|
-          if response.code == 200
-            flash[:notice] = "Activating MSOA #{msoa_code}."
-          else
-            flash[:error] = "Unable to activate MSOA #{msoa_code} (HTTP #{response.code} received)."
-          end
-        end
-
-        msoas_url = "/manage/regions/#{region_code}/las/#{local_authority_code}/msoas"
-        msoas_url += "?page=#{params[:page]}" if params[:page].present?
-        redirect msoas_url
-      end
-
-      # Present a form for issuing a Drools command.
-      ['/drools/?', '/manage/drools?'].each do |path|
-        get path do
-          authenticate!
-          erb :drools, locals: { title: 'Run Drools Rules' }
-        end
-      end
-
-      # Issue a Drools command.
-      post '/manage/drools' do
-        authenticate!
-        command = params[:command]
-        filter_code = params[:factfiltercode]
-        RestClient.post("http://#{settings.follow_up_service_host}:#{settings.follow_up_service_port}/FollowUpService/IssueDroolsCommand",
-                        { commandName: command, factFilterCode: filter_code }.to_json, content_type: :json, accept: :json) do |response, _request, _result, &_block|
-          if response.code == 200
-            flash[:notice] = "Successfully issued Drools command for Ruleset #{command} and LA Code #{filter_code}."
-          else
-            flash[:notice] = "Unable to issue Drools command (HTTP #{response.code} received)."
-          end
-        end
-
-        redirect '/manage/drools'
-      end
-
-      # Get information about the Redmine gateway for follow-ups.
-      ['/gateway/?', '/manage/gateway/?'].each do |path|
-        get path do
-          authenticate!
-          gateway = Gateway.new(JSON.parse(RestClient.get("http://#{settings.follow_up_service_host}:#{settings.follow_up_service_port}/FollowUpService/GatewayInfo")))
-          erb :gateway, locals: { title: 'Redmine Gateway Status', gateway: gateway }
-        end
-      end
-
-      # Toggle the started/stopped state of the Redmine gateway for follow-ups.
-      post '/manage/gateway' do
-        authenticate!
-        active = !(params[:active] == 'true') ? true : false
-
-        # A date has to be passed for higheststamp and last stamp but it's not actually used.
-        epoch = '1970-01-01T00:00:00Z'
-        RestClient.put("http://#{settings.follow_up_service_host}:#{settings.follow_up_service_port}/FollowUpService/GatewayInfo",
-                       { active: active,
-                         higheststamp: epoch,
-                         laststamp: epoch }.to_json, content_type: :json, accept: :json) do |response, _request, _result, &_block|
-          if response.code == 200
-            flash[:notice] = "#{(active) ? 'Started' : 'Stopped'} polling the gateway."
-          else
-            flash[:notice] = "Unable to #{(active) ? 'start' : 'stop'} polling the gateway (HTTP #{response.code} received)."
-          end
-        end
-
-        redirect '/manage/gateway'
-      end
     end
   end
 end
