90ea6c35c4c0d76a1ecefbecc4185b3ae793814f
John Topley <john.topley@ons.gov.uk>
2016-07-26 16:16:05 +0100

    CTPA-623 Better variable name

--- routes/manage.rb
+++ routes/manage.rb
@@ -6,14 +6,14 @@ get '/manage' do
 end
 
 # Get all escalated cases.
-get '/manage/escalated/:escalation_type' do |escalation_type|
-  escalated = []
+get '/manage/escalated/:escalation_type' do |escalation_type|  
   authenticate!
+  escalations = []
   type = "#{escalation_type.capitalize}Escalation"
 
   RestClient.get("http://#{settings.action_service_host}:#{settings.action_service_port}/actions?actiontype=#{type}&state=PENDING") do |response, _request, _result, &_block|
-    escalated = JSON.parse(response).paginate(page: params[:page]) unless response.code == 204
+    escalations = JSON.parse(response).paginate(page: params[:page]) unless response.code == 204
   end
 
-  erb :escalated_cases, locals: { title: "View Escalated #{escalation_type.capitalize} Cases", escalated: escalated }
+  erb :escalated_cases, locals: { title: "View Escalated #{escalation_type.capitalize}", escalations: escalations }
 end
