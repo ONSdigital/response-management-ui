2182b6361f46762df0ffb770eb127033ee2b6000
Kieran Wardle <kieran.wardle@ons.gov.uk>
2017-02-06 14:50:29 +0000

    CTPA-1105 Fixed rubocop formatting errors

--- routes/reports.rb
+++ routes/reports.rb
@@ -10,28 +10,28 @@ get '/reports' do
   action_report_types = []
   RestClient.get("#{settings.protocol}://#{settings.case_service_host}:#{settings.case_service_port}/reports/types") do |response, _request, _result, &_block|
     case_report_types = JSON.parse(response) unless response.code == 204
-    (0..case_report_types.size-1).each do |i|
+    (0..case_report_types.size - 1).each do |i|
       case_report_types[i]['reportClass'] = 'case'
     end
   end
   RestClient::Request.execute(method: :get,
-                                url: "#{settings.protocol}://#{settings.action_exporter_host}:#{settings.action_exporter_port}/reports/types",
-                                user: settings.action_exporter_user,
-                                password: settings.action_exporter_password) do |response, _request, _result, &_block|
-      action_report_types = JSON.parse(response) unless response.code == 204
-      url = '/'
-     (0..action_report_types.size-1).each do |i|
-         action_report_types[i]['reportClass'] = 'action'
-     end
-   end
+                              url: "#{settings.protocol}://#{settings.action_exporter_host}:#{settings.action_exporter_port}/reports/types",
+                              user: settings.action_exporter_user,
+                              password: settings.action_exporter_password) do |response, _request, _result, &_block|
+    action_report_types = JSON.parse(response) unless response.code == 204
+    url = '/'
+    (0..action_report_types.size - 1).each do |i|
+      action_report_types[i]['reportClass'] = 'action'
+    end
+  end
   report_types = case_report_types + action_report_types
   report_types = report_types.paginate(page: params[:page])
   erb :reports, locals: { title: 'Reports',
                           user: user_role,
-                          report_types: report_types}
+                          report_types: report_types }
 end
 
-get '/reports/:report_class/:report_type' do |report_class,report_type|
+get '/reports/:report_class/:report_type' do |report_class, report_type|
   authenticate!
   report_details = []
   error = 0
@@ -40,19 +40,19 @@ get '/reports/:report_class/:report_type' do |report_class,report_type|
   port = settings.report_ports[report_class]
   auth = settings.report_auth[report_class]
 
-    RestClient::Request.execute(method: :get,
-                                  url: "#{settings.protocol}://#{host}:#{port}/reports/types/#{report_type.upcase}",
-                                  user: settings.action_exporter_user,
-                                  password: settings.action_exporter_password) do |response, _request, _result, &_block|
-      report_details = JSON.parse(response).paginate(page: params[:page]) unless response.code == 204 || response.code == 400
-      code = response.code
-    end
+  RestClient::Request.execute(method: :get,
+                              url: "#{settings.protocol}://#{host}:#{port}/reports/types/#{report_type.upcase}",
+                              user: settings.action_exporter_user,
+                              password: settings.action_exporter_password) do |response, _request, _result, &_block|
+    report_details = JSON.parse(response).paginate(page: params[:page]) unless response.code == 204 || response.code == 400
+    code = response.code
+  end
   if code == 200 || code == 204
     erb :report_type, locals: { title: type_format(report_type),
                                 user: user_role,
                                 report_details: report_details,
                                 report_type: report_type,
-                                report_class: report_class}
+                                report_class: report_class }
   else
     erb :reports_errors, locals: { title: 'Error!',
                                    user: user_role,
@@ -61,38 +61,38 @@ get '/reports/:report_class/:report_type' do |report_class,report_type|
   end
 end
 
-get '/reports/download/:report_class/:report_id' do |report_class,report_id|
+get '/reports/download/:report_class/:report_id' do |report_class, report_id|
   authenticate!
   report_details = []
   host = settings.report_hosts[report_class]
   port = settings.report_ports[report_class]
 
-    RestClient::Request.execute(method: :get,
-                                  url: "#{settings.protocol}://#{host}:#{port}/reports/#{report_id}",
-                                  user: settings.action_exporter_user,
-                                  password: settings.action_exporter_password) do |response, _request, _result, &_block|
-      report_details = JSON.parse(response) unless response.code == 204 || response.code == 400
-    end
+  RestClient::Request.execute(method: :get,
+                              url: "#{settings.protocol}://#{host}:#{port}/reports/#{report_id}",
+                              user: settings.action_exporter_user,
+                              password: settings.action_exporter_password) do |response, _request, _result, &_block|
+    report_details = JSON.parse(response) unless response.code == 204 || response.code == 400
+  end
   t = Time.parse(report_details['createdDateTime'])
   t = t.localtime.strftime('%Y%m%d')
   attachment "#{type_format(report_details['reportType']).tr(' ', '_')}_#{t}.csv"
   report_details['contents']
 end
 
-get '/reports/view/:report_class/:report_id' do |report_class,report_id|
+get '/reports/view/:report_class/:report_id' do |report_class, report_id|
   authenticate!
   report_details = []
   error = 1
   host = settings.report_hosts[report_class]
   port = settings.report_ports[report_class]
-   code = 0
-    RestClient::Request.execute(method: :get,
-                                  url: "#{settings.protocol}://#{host}:#{port}/reports/#{report_id}",
-                                  user: settings.action_exporter_user,
-                                  password: settings.action_exporter_password) do |response, _request, _result, &_block|
-      report_details = JSON.parse(response) unless response.code == 204 || response.code == 400
-      code = response.code
-    end
+  code = 0
+  RestClient::Request.execute(method: :get,
+                              url: "#{settings.protocol}://#{host}:#{port}/reports/#{report_id}",
+                              user: settings.action_exporter_user,
+                              password: settings.action_exporter_password) do |response, _request, _result, &_block|
+    report_details = JSON.parse(response) unless response.code == 204 || response.code == 400
+    code = response.code
+  end
   if code == 200
     contents = CSV.parse(report_details['contents'], headers: false).to_a
     erb :view_report, locals: { title: type_format(report_details['reportType']) + ' ' + report_details['createdDateTime'].to_report_date,
