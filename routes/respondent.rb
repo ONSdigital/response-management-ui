53ef3efd96ede394c8061422821d2781f2654735
Edward Stevens <edward.stevens@ons.gov.uk>
2017-07-17 15:02:47 +0100

    CTPA-1406 Added initial work for function to modify user email address and send confirmation email via notify gateway

--- routes/respondent.rb
+++ routes/respondent.rb
@@ -1,6 +1,4 @@
-
-
-get '/respondents/:respondentid' do | respondentid |
+get '/respondents/:respondentid' do |respondentid|
 
   respondent = []
 
@@ -8,6 +6,31 @@ get '/respondents/:respondentid' do | respondentid |
     respondent = JSON.parse(respondent_response) unless respondent_response.code == 404
   end
 
-  erb :respondent, locals: { title: 'Respondents',
-                          respondent: respondent }
+  puts respondent
+
+  erb :respondent, locals: {  title: 'Respondents',
+                              respondent: respondent }
+end
+
+post '/respondents/amendemail' do
+
+  email_template_id = '290b93f2-04c2-413d-8f9b-93841e684e90'
+
+  # Pass through emailaddress as param when endpoint updated
+  # emailaddress = params[:emailaddresstext]
+  # puts emailaddress
+
+  RestClient.post("#{settings.protocol}://#{settings.notifygateway_host}:#{settings.notifygateway_port}/notify/emails/#{email_template_id}",
+                  {}.to_json, content_type: :json, accept: :json) do |post_response, _request, _result, &_block|
+
+    if post_response.code == 201
+      flash[:notice] = 'Successfully amended email.'
+      actions = []
+    else
+      logger.error post_response
+      error_flash('Unable to amend email', post_response)
+    end
+  end
+
+  erb :emailamended, locals: { title: 'Email Amended' }
 end
