730bbb80007c628b2efd10d80476cbdd4e0b359c
Edward Stevens <edward.stevens@ons.gov.uk>
2017-08-21 14:59:09 +0100

    Removed search box submit button javascript

--- public/javascripts/signin.js
+++ public/javascripts/signin.js
@@ -1,14 +1,3 @@
 $(document).ready(function(event) {
   $("#username").focus();
 });
-
-$(document).ready(function() {
-    $("#search-form").submit(function( event ) {
-      var sampleunitref = $("#sampleunitref").val();
-      if (sampleunitref.length > 0) {
-        window.location = "/sampleunitref/" + sampleunitref + "/cases";
-      }
-      event.preventDefault();
-    });
-
-});
