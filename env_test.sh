81351d0c80b0f3301e67a64289d913b0b82c9382
Edward Stevens <edward.stevens@ons.gov.uk>
2017-08-30 14:31:32 +0100

    Added env_test.sh file

---
+++ env_test.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#
+# Script for setting the required environment variables in testegration.
+# Adjust as required.
+#
+# Usage: . ./env_test.sh
+#
+# Author: Edward Stevens (edward.stevens@ons.gov.uk)
+#
+export BRES_2017_COLLECTION_EXERCISE="14fb3e68-4dca-46db-bf49-04b84e07e77c"
+export RAS_BACKSTAGE_UI_COLLECTION_EXERCISES="collection-exercises"
+export RAS_BACKSTAGE_UI_HTTP_PROTOCOL="http"
+export RAS_BACKSTAGE_UI_HOST="ras-backstage-test.apps.devtest.onsclofo.uk"
+export RESPONSE_OPERATIONS_ACTION_SERVICE_HOST="actionsvc-test.apps.devtest.onsclofo.uk"
+export RESPONSE_OPERATIONS_ACTION_SERVICE_PORT="80"
+export RESPONSE_OPERATIONS_ACTIONEXPORTER_SERVICE_HOST="actionexportersvc-test.apps.devtest.onsclofo.uk"
+export RESPONSE_OPERATIONS_ACTIONEXPORTER_SERVICE_PORT="80"
+export RESPONSE_OPERATIONS_CASE_SERVICE_HOST="casesvc-test.apps.devtest.onsclofo.uk"
+export RESPONSE_OPERATIONS_CASE_SERVICE_PORT="80"
+export RESPONSE_OPERATIONS_COLLECTION_EXERCISE_SERVICE_HOST="collectionexercisesvc-test.apps.devtest.onsclofo.uk"
+export RESPONSE_OPERATIONS_COLLECTION_EXERCISE_SERVICE_PORT="80"
+export RESPONSE_OPERATIONS_SAMPLE_SERVICE_HOST="samplesvc-test.apps.devtest.onsclofo.uk"
+export RESPONSE_OPERATIONS_SAMPLE_SERVICE_PORT="80"
+export RESPONSE_OPERATIONS_EMAIL_TEMPLATE_ID="53c59576-8c3b-4298-99d1-b245ddd28500"
+export RESPONSE_OPERATIONS_HTTP_PROTOCOL="http"
+export RESPONSE_OPERATIONS_NOTIFYGATEWAY_SERVICE_HOST="notifygatewaysvc-test.apps.devtest.onsclofo.uk"
+export RESPONSE_OPERATIONS_NOTIFYGATEWAY_SERVICE_PORT="80"
+export RESPONSE_OPERATIONS_OAUTHSERVER_HOST="http://ras-django-test.apps.devtest.onsclofo.uk/api/v1/tokens"
+export RESPONSE_OPERATIONS_PARTY_SERVICE_HOST="ras-party-service-test.apps.devtest.onsclofo.uk"
+export RESPONSE_OPERATIONS_PARTY_SERVICE_PORT="80"
+export RESPONSE_OPERATIONS_SECURE_MESSAGE_SERVICE_HOST="ras-backstage-test.apps.devtest.onsclofo.uk/secure-messages"
+export RESPONSE_OPERATIONS_SECURE_MESSAGE_SERVICE_PORT="80"
