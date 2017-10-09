ee4d3a8a82d9bb80338b42f5b109884bdb2afae8
Phil Sharland <philip.sharland@ons.gov.uk>
2017-10-09 10:46:00 +0100

    add env_sit.sh file

---
+++ env_sit.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#
+# Script for setting the required environment variables in integration.
+# Adjust as required.
+#
+# Usage: . ./env_sit.sh
+#
+# Author: Philip Sharland (philip.sharland@ons.gov.uk)
+#
+export BRES_2017_COLLECTION_EXERCISE="14fb3e68-4dca-46db-bf49-04b84e07e77c"
+export RAS_BACKSTAGE_UI_COLLECTION_EXERCISES="collection-exercises"
+export RAS_BACKSTAGE_UI_PROTOCOL="http"
+export RAS_BACKSTAGE_UI_HOST="ras-backstage-sit.apps.devtest.onsclofo.uk"
+export RESPONSE_OPERATIONS_ACTION_SERVICE_HOST="actionsvc-sit.apps.devtest.onsclofo.uk"
+export RESPONSE_OPERATIONS_ACTION_SERVICE_PORT="80"
+export RESPONSE_OPERATIONS_ACTIONEXPORTER_SERVICE_HOST="actionexportersvc-sit.apps.devtest.onsclofo.uk"
+export RESPONSE_OPERATIONS_ACTIONEXPORTER_SERVICE_PORT="80"
+export RESPONSE_OPERATIONS_CASE_SERVICE_HOST="casesvc-sit.apps.devtest.onsclofo.uk"
+export RESPONSE_OPERATIONS_CASE_SERVICE_PORT="80"
+export RESPONSE_OPERATIONS_SAMPLE_SERVICE_HOST="samplesvc-sit.apps.devtest.onsclofo.uk"
+export RESPONSE_OPERATIONS_SAMPLE_SERVICE_PORT="80"
+export RESPONSE_OPERATIONS_COLLECTION_EXERCISE_SERVICE_HOST="collectionexercisesvc-sit.apps.devtest.onsclofo.uk"
+export RESPONSE_OPERATIONS_COLLECTION_EXERCISE_SERVICE_PORT="80"
+export RESPONSE_OPERATIONS_HTTP_PROTOCOL="http"
+export RESPONSE_OPERATIONS_NOTIFYGATEWAY_SERVICE_HOST="notifygatewaysvc-sit.apps.devtest.onsclofo.uk"
+export RESPONSE_OPERATIONS_NOTIFYGATEWAY_SERVICE_PORT="80"
+export RESPONSE_OPERATIONS_OAUTHSERVER_HOST="http://ras-django-sit.apps.devtest.onsclofo.uk/api/v1/tokens"
+export RESPONSE_OPERATIONS_PARTY_SERVICE_HOST="ras-party-service-sit.apps.devtest.onsclofo.uk"
+export RESPONSE_OPERATIONS_PARTY_SERVICE_PORT="80"
+export RESPONSE_OPERATIONS_SECURE_MESSAGE_SERVICE_HOST="ras-backstage-sit.apps.devtest.onsclofo.uk/secure-messages"
+export RESPONSE_OPERATIONS_SECURE_MESSAGE_SERVICE_PORT="80"
