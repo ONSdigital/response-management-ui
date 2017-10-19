eeabf3c4c04c915c89d12793810c0a002eacc6f1
Edward Stevens <edward.stevens@ons.gov.uk>
2017-10-19 13:41:00 +0100

    CTPA-1645 Split collection exercise association into own screen

--- routes/sample_upload.rb
+++ routes/sample_upload.rb
@@ -5,11 +5,15 @@ get '/sample_upload' do
 
   RestClient::Request.execute(method: :get,
                               url: "#{settings.protocol}://#{settings.survey_service_host}:#{settings.survey_service_port}/surveys",
-                              user: settings.security_user_name,
-                              password: settings.security_user_password,
+                              #user: settings.security_user_name,
+                              #password: settings.security_user_password,
+                              user: 'admin',
+                              password: 'secret',
                               realm: settings.security_realm) do |surveys_response, _request, _result, &_block|
     surveys = JSON.parse(surveys_response) unless surveys_response.code == 404
   end
+
+  puts 'surveys: ' + surveys.to_s
 
   erb :sample_upload, locals: { title: 'Sample Upload',
                                 surveys: surveys }
@@ -17,31 +21,34 @@ end
 
 post '/sample_upload' do
 
-  sample_file_path = params[:sample_file][:tempfile].path
-  survey_type = params[:survey_type]
+    sample_file_path = params[:sample_file][:tempfile].path
+    survey_type = params[:survey_type]
+    event_url = ''
 
-  RestClient::Request.execute(method: :post,
-                              url: "#{settings.protocol}://#{settings.sample_service_host}:#{settings.sample_service_port}/samples/#{survey_type}/fileupload",
-                              user: settings.security_user_name,
-                              password: settings.security_user_password,
-                              realm: settings.security_realm,
-                              payload: {
-                                multipart: true,
-                                file: File.new(sample_file_path, 'r')
-                              }) do |post_response_event, _request, _result, &_block|
+    RestClient::Request.execute(method: :post,
+                                url: "#{settings.protocol}://#{settings.sample_service_host}:#{settings.sample_service_port}/samples/#{survey_type}/fileupload",
+                                #user: settings.security_user_name,
+                                #password: settings.security_user_password,
+                                user: 'admin',
+                                password: 'secret',
+                                realm: settings.security_realm,
+                                payload: {
+                                  multipart: true,
+                                  file: File.new(sample_file_path, 'r')
+                                }) do |post_response_event, _request, _result, &_block|
 
-    if post_response_event.code == 201
-      flash[:notice] = 'Sample successfully uploaded.'
-    else
+      if post_response_event.code == 201
+        flash[:notice] = 'Sample successfully uploaded.'
+        event_url = '/associate_collection_exercise'
+      else
+        puts post_response_event.to_s
+        logger.error post_response_event
+        error_flash('Error uploading sample', post_response_event)
+        event_url = '/sample_upload'
+      end
 
-      puts post_response_event.to_s
-      logger.error post_response_event
-      error_flash('Error uploading sample', post_response_event)
     end
 
-    event_url = '/sample_upload'
     redirect event_url
-
-  end
 
 end
