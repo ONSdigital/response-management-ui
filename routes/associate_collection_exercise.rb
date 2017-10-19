d091ffe20c170b758ca7f7c074841ef925b9c7dd
Edward Stevens <edward.stevens@ons.gov.uk>
2017-10-19 14:34:48 +0100

    Rubocop fixes

--- routes/associate_collection_exercise.rb
+++ routes/associate_collection_exercise.rb
@@ -5,48 +5,40 @@ get '/associate_collection_exercise' do
 
   RestClient::Request.execute(method: :get,
                               url: "#{settings.protocol}://#{settings.collection_exercise_service_host}:#{settings.collection_exercise_service_port}/collectionexercises/",
-                              #user: settings.security_user_name,
-                              #password: settings.security_user_password,
-                              user: 'admin',
-                              password: 'secret',
+                              user: settings.security_user_name,
+                              password: settings.security_user_password,
                               realm: settings.security_realm) do |collectionexercise_response, _request, _result, &_block|
     collectionexercises = JSON.parse(collectionexercise_response) unless collectionexercise_response.code == 404
   end
 
-  puts 'collectionexercises: ' + collectionexercises.to_s
-
   erb :associate_collection_exercise, locals: { title: 'Associate Collection Exercise',
-                              collectionexercises: collectionexercises }
+                                                collectionexercises: collectionexercises }
 end
 
 post '/associate_collection_exercise' do
 
-    collectionexercise = params[:collectionexercise]
-    puts 'collectionexercise: ' + collectionexercise.to_s
-    puts "#{settings.protocol}://#{settings.collection_exercise_service_host}:#{settings.collection_exercise_service_port}/collectionexercises/#{collectionexercise}"
+  collectionexercise = params[:collectionexercise]
 
+  RestClient::Request.execute(method: :put,
+                              url: "#{settings.protocol}://#{settings.collection_exercise_service_host}:#{settings.collection_exercise_service_port}/collectionexercises/#{collectionexercise}",
+                              user: settings.security_user_name,
+                              password: settings.security_user_password,
+                              realm: settings.security_realm,
+                              payload: {},
+                              headers: { 'Content-Type' => 'application/json' },
+                              accept: :json) do |put_response_event, _request, _result, &_block|
 
-    RestClient::Request.execute(method: :put,
-                                url: "#{settings.protocol}://#{settings.collection_exercise_service_host}:#{settings.collection_exercise_service_port}/collectionexercises/#{collectionexercise}",
-                                #user: settings.security_user_name,
-                                #password: settings.security_user_password,
-                                user: 'admin',
-                                password: 'secret',
-                                realm: settings.security_realm,
-                                payload: {},
-                                headers: { 'Content-Type' => 'application/json' },
-                                accept: :json) do |put_response_event, _request, _result, &_block|
-                                  if put_response_event.code == 200
-                                    flash[:notice] = 'Collection Exercise successfully associated.'
-                                  else
-                                    puts put_response_event.to_s
-                                    logger.error put_response_event
-                                    error_flash('Error associating sample to Collection Exercise', put_response_event)
-                                  end
-
-                                  event_url = '/associate_collection_exercise'
-                                  redirect event_url
-
+    if put_response_event.code == 200
+      flash[:notice] = 'Collection Exercise successfully associated.'
+    else
+      puts put_response_event.to_s
+      logger.error put_response_event
+      error_flash('Error associating sample to Collection Exercise', put_response_event)
     end
+
+    event_url = '/associate_collection_exercise'
+    redirect event_url
+
+  end
 
 end
